c6af958bf53375af9398df4cc5dada7d547236f8
CSilivestru <chris.silivestru@gmail.com>
2013-04-10 16:44:25 -0500

    Simplified the popup js code

--- public/js/components/OpenLayersPopup.js
+++ public/js/components/OpenLayersPopup.js
@@ -4,13 +4,9 @@ define([
        'backbone',
 ], function($, _, Backbone){
 
-    /*
-     * Normal OpenLayers popups are anchored at the corners. I would
-     * prefer them to be anchored on the middle of the side.
-     */
-    OpenLayers.Popup.SideAnchored =
+    //Adapted from the leflets example at openlayers.org
+    OpenLayers.Popup.TipFrame =
       OpenLayers.Class(OpenLayers.Popup, {
-
         /**
          * Property: relativePosition
          * {String} Relative position of the popup ("t", "r", "b" or "l").
@@ -44,66 +40,6 @@ define([
          */
         anchor: null,
 
-        /**
-        * Constructor: OpenLayers.Popup.SideAnchored
-        *
-        * Parameters:
-        * id - {String}
-        * lonlat - {<OpenLayers.LonLat>}
-        * contentSize - {<OpenLayers.Size>}
-        * contentHTML - {String}
-        * anchor - {Object} Object which must expose a 'size' <OpenLayers.Size>
-        *     and 'offset' <OpenLayers.Pixel> (generally an <OpenLayers.Icon>).
-        * closeBox - {Boolean}
-        * closeBoxCallback - {Function} Function to be called on closeBox click.
-        */
-        initialize:function(id, lonlat, contentSize, contentHTML, anchor, closeBox,
-                            closeBoxCallback) {
-            var newArguments = [
-                id, lonlat, contentSize, contentHTML, closeBox, closeBoxCallback
-            ];
-            OpenLayers.Popup.prototype.initialize.apply(this, newArguments);
-
-            this.anchor = (anchor != null) ? anchor
-                                           : { size: new OpenLayers.Size(0,0),
-                                               offset: new OpenLayers.Pixel(0,0)};
-        },
-
-        /**
-         * APIMethod: destroy
-         */
-        destroy: function() {
-            this.anchor = null;
-            this.relativePosition = null;
-
-            OpenLayers.Popup.prototype.destroy.apply(this, arguments);
-        },
-
-        /**
-         * APIMethod: show
-         * Overridden from Popup since user might hide popup and then show() it
-         *     in a new location (meaning we might want to update the relative
-         *     position on the show)
-         */
-        show: function() {
-            this.updatePosition();
-            OpenLayers.Popup.prototype.show.apply(this, arguments);
-        },
-
-        /**
-         * Method: moveTo
-         * Since the popup is moving to a new px, it might need also to be moved
-         *     relative to where the marker is. We first calculate the new
-         *     relativePosition, and then we calculate the new px where we will
-         *     put the popup, based on the new relative position.
-         *
-         *     If the relativePosition has changed, we must also call
-         *     updateRelativePosition() to make any visual changes to the popup
-         *     which are associated with putting it in a new relativePosition.
-         *
-         * Parameters:
-         * px - {<OpenLayers.Pixel>}
-         */
         moveTo: function(px) {
             var oldRelativePosition = this.relativePosition;
             this.relativePosition = this.calculateRelativePosition(px);
@@ -120,32 +56,6 @@ define([
             }
         },
 
-        /**
-         * APIMethod: setSize
-         *
-         * Parameters:
-         * contentSize - {<OpenLayers.Size>} the new size for the popup's
-         *     contents div (in pixels).
-         */
-        setSize:function(contentSize) {
-            OpenLayers.Popup.prototype.setSize.apply(this, arguments);
-
-            if ((this.lonlat) && (this.map)) {
-                var px = this.map.getLayerPxFromLonLat(this.lonlat);
-                this.moveTo(px);
-            }
-        },
-
-        /**
-         * Method: calculateRelativePosition
-         *
-         * Parameters:
-         * px - {<OpenLayers.Pixel>}
-         *
-         * Returns:
-         * {String} The relative position ("t" "r" "b" "l") at which the popup
-         *     should be placed.
-         */
         calculateRelativePosition:function(px) {
             var determineThird = function(bounds, lonlat) {
                 var thirds = ["tmb", "lcr"];
@@ -204,68 +114,6 @@ define([
 
             return map[this.orientation](quadrant, third);
         },
-
-        /**
-         * Method: updateRelativePosition
-         * The popup has been moved to a new relative location, so we may want to
-         *     make some cosmetic adjustments to it.
-         *
-         *     Note that in the classic SideAnchored popup, there is nothing to do
-         *     here, since the popup looks exactly the same in all four positions.
-         *     Subclasses such as Framed, however, will want to do something
-         *     special here.
-         */
-        updateRelativePosition: function() {
-            //to be overridden by subclasses
-        },
-
-        /**
-         * Method: calculateNewPx
-         *
-         * Parameters:
-         * px - {<OpenLayers.Pixel>}
-         *
-         * Returns:
-         * {<OpenLayers.Pixel>} The the new px position of the popup on the screen
-         *     relative to the passed-in px.
-         */
-        calculateNewPx:function(px) {
-            var newPx = px.offset(this.anchor.offset);
-
-            //use contentSize if size is not already set
-            var size = this.size || this.contentSize;
-
-            if (this.relativePosition == "t" || this.relativePosition == "b") {
-                newPx.x += (this.anchor.size.w - size.w) / 2.0;
-
-                if (this.relativePosition == "t") {
-                    newPx.y -= size.h;
-                } else {
-                    newPx.y += this.anchor.size.h;
-                }
-            } else if (this.relativePosition == "l" || this.relativePosition == "r") {
-                newPx.y += (this.anchor.size.h - size.h) / 2.0;
-
-                if (this.relativePosition == "l") {
-                    newPx.x -= size.w;
-                } else {
-                    newPx.x += this.anchor.size.w;
-                }
-            }
-
-            return newPx;
-        },
-
-        CLASS_NAME: "OpenLayers.Popup.SideAnchored"
-    });
-
-    /*
-     * This popup is supposed to look like the popup from Cloudmade's
-     * Leaflet mapping library -- i.e. better than the openlayers
-     * popups. It only just barely works.
-     */
-    OpenLayers.Popup.TipFrame =
-      OpenLayers.Class(OpenLayers.Popup.SideAnchored, {
 
         /**
          * Property: positionBlocks
@@ -349,7 +197,14 @@ define([
         initialize:function(id, lonlat, contentSize, contentHTML, anchor, closeBox,
                             closeBoxCallback, position) {
 
-            OpenLayers.Popup.SideAnchored.prototype.initialize.apply(this, arguments);
+            var newArguments = [
+                id, lonlat, contentSize, contentHTML, closeBox, closeBoxCallback
+            ];
+            OpenLayers.Popup.prototype.initialize.apply(this, newArguments);
+
+            this.anchor = (anchor != null) ? anchor
+                                           : { size: new OpenLayers.Size(0,0),
+                                               offset: new OpenLayers.Pixel(0,0)};
 
             if (position) {
                 this.fixedRelativePosition = true;
@@ -416,11 +271,14 @@ define([
             }
             this.blocks = null;
 
-            OpenLayers.Popup.SideAnchored.prototype.destroy.apply(this, arguments);
+            this.anchor = null;
+            this.relativePosition = null;
+
+            OpenLayers.Popup.prototype.destroy.apply(this, arguments);
         },
 
         draw: function(px) {
-            div = OpenLayers.Popup.SideAnchored.prototype.draw.apply(this, arguments);
+            div = OpenLayers.Popup.prototype.draw.apply(this, arguments);
 
             // undo overflow: hidden hacking
             // fixme: should just make div big enough for everything?
@@ -468,7 +326,12 @@ define([
          *     contents div (in pixels).
          */
         setSize:function(contentSize) {
-            OpenLayers.Popup.SideAnchored.prototype.setSize.apply(this, arguments);
+            OpenLayers.Popup.prototype.setSize.apply(this, arguments);
+
+            if ((this.lonlat) && (this.map)) {
+                var px = this.map.getLayerPxFromLonLat(this.lonlat);
+                this.moveTo(px);
+            }
 
             this.updateBlocks();
         },
@@ -511,9 +374,28 @@ define([
          *     relative to the passed-in px.
          */
         calculateNewPx:function(px) {
-            var newPx = OpenLayers.Popup.SideAnchored.prototype.calculateNewPx.apply(
-                this, arguments
-            );
+            var newPx = px.offset(this.anchor.offset);
+
+            //use contentSize if size is not already set
+            var size = this.size || this.contentSize;
+
+            if (this.relativePosition == "t" || this.relativePosition == "b") {
+                newPx.x += (this.anchor.size.w - size.w) / 2.0;
+
+                if (this.relativePosition == "t") {
+                    newPx.y -= size.h;
+                } else {
+                    newPx.y += this.anchor.size.h;
+                }
+            } else if (this.relativePosition == "l" || this.relativePosition == "r") {
+                newPx.y += (this.anchor.size.h - size.h) / 2.0;
+
+                if (this.relativePosition == "l") {
+                    newPx.x -= size.w;
+                } else {
+                    newPx.x += this.anchor.size.w;
+                }
+            }
 
             newPx = newPx.offset(this.positionBlocks[this.relativePosition].offset);
 
