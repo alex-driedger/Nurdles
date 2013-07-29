eb967a95a8642ef36036c2aa15d2d3d0578d59a4
Rob Drimmie <rob@drimmie.net>
2013-07-29 10:18:43 -0400

    Removed some more ee specific details

--- db/access/statedal.js
+++ db/access/statedal.js
@@ -1,32 +1,7 @@
-var Filter = require('../models/Filter').Filter,
-    User = require('../models/User').User,
+var User = require('../models/User').User,
     mongoose = require('../index').getMongoose();
 
 var self = {
-    saveFilterState: function(userId, activeFilters, callback) {
-        var ObjectId = mongoose.Types.ObjectId;
-        User.findById(new ObjectId(userId), function(err, user) {
-            if (err)
-                callback(err);
-            else {
-                user.activeFilters = activeFilters;
-                user.save();
-                callback(null, user.activeFilters);
-            }
-        });
-    },
-
-    getFilterState: function(userId, callback) {
-        var ObjectId = mongoose.Types.ObjectId;
-
-        User.findById(new ObjectId(userId), function(err, user) {
-            if (err)
-                callback(err);
-            else
-                callback(null, user.activeFilters);
-        });
-    }
 };
 
 module.exports = self;
-
--- routes/statemanager.js
+++ routes/statemanager.js
@@ -1,31 +1,7 @@
-var filterdal = require('../db/access/filterdal.js'),
-    userdal = require('../db/access/userdal.js'),
+var userdal = require('../db/access/userdal.js'),
     statedal = require('../db/access/statedal.js');
 
 var self = {
-    saveFilterState: function(req, res) {
-        statedal.saveFilterState(req.user._id.toString(), req.body.activeFilters, function(err, response) {
-            if (err) {
-                res.statusCode = 500;
-                res.send({error: err});
-            }
-            else {
-                res.send(response);
-            }
-        });
-    },
-
-    getFilterState: function(req, res) {
-        statedal.getFilterState(req.user._id.toString(), function(err, activeFilters) {
-            if (err) {
-                res.statusCode = 500;
-                res.send({error: err});
-            }
-            else {
-                res.send(activeFilters);
-            }
-        });
-    }
 };
 
 module.exports = self;
