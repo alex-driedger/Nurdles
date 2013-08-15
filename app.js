80943157d3d04d31ac964c57f6280201721ed132
Rob Drimmie <rob@drimmie.net>
2013-08-15 15:17:18 -0400

    Removed xmlBodyParser because I don't know what it's for and I can't see anywhere it's being used. My expectation is that I will have to add it in again - and probably be really confused for a while before I realize it's necessary - but at least I'll understand, should that come to pass.

--- app.js
+++ app.js
@@ -7,43 +7,11 @@ var express = require('express'),
     utils = require('connect').utils, 
     parser = require('xml2js');
 
-function xmlBodyParser(req, res, next) {
-    if (req._body) return next();
-    req.body = req.body || {};
-
-    // ignore GET
-    if ('GET' == req.method || 'HEAD' == req.method) return next();
-
-    // check Content-Type
-    if ('application/xml' != utils.mime(req)) return next();
-
-    // flag as parsed
-    req._body = true;
-
-    // parse
-    var buf = '';
-    req.setEncoding('utf8');
-    req.on('data', function(chunk){ buf += chunk });
-    req.on('end', function(){  
-        parser.parseString(buf, function(err, json) {
-            if (err) {
-                err.status = 400;
-                next(err);
-            } else {
-                req.body = json;
-                req.xml = buf;
-                next();
-            }
-        });
-    });
-}
-
 function run(opts) {
     var app = express(),
         port = opts.port || conf.ports.server;
        
     app.configure(function() {
-        app.use(xmlBodyParser),
         app.use(express.bodyParser()),
         app.use(express.static(conf.dirs.client));
         app.use(express.methodOverride());
